b144b055b333258dc18630248367a0a0e0ce6eb1
amin10 <manna@mit.edu>
2016-09-17 15:05:10 -0400

    implemented export command. Fixed args so that it's a string not an array

--- src/background.js
+++ src/background.js
@@ -1,15 +1,23 @@
+var local = {}
 var commands = {
   'ls' : function(args) {
     alert('ls ' +args);
   },
   'cd' : function(args) {
     alert('cd ' + args);
+  },
+  'export' : function(args) {
+    var tokens = _.map(args.split("="), _.trim);
+    var variable=tokens[0];
+    var value=tokens[1];
+    local[variable] = value;
   }
 };
 
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
     console.log('inputChanged: ' + text);
+    console.log(local);
     suggest([
       {content: text + " one", description: "the first one"},
       {content: text + " number two", description: "the second entry"}
@@ -19,8 +27,9 @@ chrome.omnibox.onInputChanged.addListener(
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
-    var cmd = text.split(" ")[0];
-    var args = text.split(" ").slice(1);
+    var words = text.split(" ");
+    var cmd = words[0];
+    var args = _.join(words.slice(1), " ");
     if (cmd in commands) {
       commands[cmd](args);
     } else {
