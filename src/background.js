eccb75a9235b7afe8da1c4bdd4c97bb75314de66
amin10 <manna@mit.edu>
2016-09-17 13:32:18 -0400

    Initial functionality commit

--- src/background.js
+++ src/background.js
@@ -1,9 +1,12 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var commands = {
+  'ls' : function(args) {
+    alert('ls', args);
+  },
+  'cd' : function(args) {
+    alert('cd', args);
+  }
+};
 
-// This event is fired each time the user updates the text in the omnibox,
-// as long as the extension's keyword mode is still active.
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
     console.log('inputChanged: ' + text);
@@ -16,6 +19,7 @@ chrome.omnibox.onInputChanged.addListener(
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
-    console.log('inputEntered: ' + text);
-    alert('You just typed "' + text + '"');
+    var cmd = text.split(" ")[0];
+    var args = text.split(" ").slice(1);
+    commands[cmd](args);
   });
